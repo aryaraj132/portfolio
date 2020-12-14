ffb8235e844deca3d47a7fd450e75d87beee68cd
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-12-14 15:37:30 +0530

    Fixing Animations

--- style.js
+++ style.js
@@ -5,9 +5,41 @@ window.onload = function() {
         document.getElementById('side-nav-header').style.visibility='visible';
     }
     let side_nav = document.getElementById('side-nav');
-    let container = document.getElementById('container');
+    let container = document.getElementById('container');    
 }
 window.addEventListener('resize', function () { 
-    "use strict";
     window.location.reload(); 
+});
+let inViewport = function(div){
+    var bounding = div.getBoundingClientRect();
+    return(
+        bounding.top <= (window.innerHeight || document.documentElement.clientHeight)-100 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+let animateRight = function(elem){
+    if (inViewport(elem)) {
+        elem.setAttribute("style", "visibility: visible;padding-left: 50px;transition: all 1s;");
+    } else {
+        elem.setAttribute("style", "visibility: hidden;padding-left: 0px;transition: all 1s;opacity:0;");
+    }
+};
+let animateUp = function(elem){
+    if(inViewport(elem)){
+        elem.setAttribute("style", "visibility: visible;padding-top: 0px;transition: all 1s;");
+    }
+    else{
+        elem.setAttribute("style", "visibility: hidden;padding-top: 50px;transition: all 1s;opacity:0;");
+    }
+}
+window.addEventListener('scroll', function () {
+    animRht = document.querySelectorAll('.animateRight');
+    for(var i=0;i<animRht.length;i++){
+        animateRight(animRht[i])
+    }
+    animUp = document.querySelectorAll('.animateUp');
+    for(var i=0;i<animUp.length;i++){
+        animateUp(animUp[i])
+    }
 });
