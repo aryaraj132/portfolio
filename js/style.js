8f07dc8b633ad195a7288a86df5590749ee83a91
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-12-14 15:47:39 +0530

    Update style.js

--- js/style.js
+++ js/style.js
@@ -6,40 +6,41 @@ window.onload = function() {
     }
     let side_nav = document.getElementById('side-nav');
     let container = document.getElementById('container');    
+
+    window.addEventListener('resize', function () { 
+        window.location.reload(); 
+    });
+    inViewport = function(div){
+        var bounding = div.getBoundingClientRect();
+        return(
+            bounding.top <= (window.innerHeight || document.documentElement.clientHeight)-100 &&
+            bounding.left >= 0 &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+    animateRight = function(elem){
+        if (inViewport(elem)) {
+            elem.setAttribute("style", "visibility: visible;padding-left: 50px;transition: all 1s;");
+        } else {
+            elem.setAttribute("style", "visibility: hidden;padding-left: 0px;transition: all 1s;opacity:0;");
+        }
+    };
+    animateUp = function(elem){
+        if(inViewport(elem)){
+            elem.setAttribute("style", "visibility: visible;padding-top: 0px;transition: all 1s;");
+        }
+        else{
+            elem.setAttribute("style", "visibility: hidden;padding-top: 50px;transition: all 1s;opacity:0;");
+        }
+    }
+    window.addEventListener('scroll', function () {
+        animRht = document.querySelectorAll('.animateRight');
+        for(var i=0;i<animRht.length;i++){
+            animateRight(animRht[i])
+        }
+        animUp = document.querySelectorAll('.animateUp');
+        for(var i=0;i<animUp.length;i++){
+            animateUp(animUp[i])
+        }
+    });
 }
-window.addEventListener('resize', function () { 
-    window.location.reload(); 
-});
-let inViewport = function(div){
-    var bounding = div.getBoundingClientRect();
-    return(
-        bounding.top <= (window.innerHeight || document.documentElement.clientHeight)-100 &&
-        bounding.left >= 0 &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-let animateRight = function(elem){
-    if (inViewport(elem)) {
-        elem.setAttribute("style", "visibility: visible;padding-left: 50px;transition: all 1s;");
-    } else {
-        elem.setAttribute("style", "visibility: hidden;padding-left: 0px;transition: all 1s;opacity:0;");
-    }
-};
-let animateUp = function(elem){
-    if(inViewport(elem)){
-        elem.setAttribute("style", "visibility: visible;padding-top: 0px;transition: all 1s;");
-    }
-    else{
-        elem.setAttribute("style", "visibility: hidden;padding-top: 50px;transition: all 1s;opacity:0;");
-    }
-}
-window.addEventListener('scroll', function () {
-    animRht = document.querySelectorAll('.animateRight');
-    for(var i=0;i<animRht.length;i++){
-        animateRight(animRht[i])
-    }
-    animUp = document.querySelectorAll('.animateUp');
-    for(var i=0;i<animUp.length;i++){
-        animateUp(animUp[i])
-    }
-});
