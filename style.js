b9569cd52eb0a540c26a7d704a4e51cfd898faaf
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-12-14 09:19:08 +0530

    First Commit

---
+++ style.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    let toggle = document.getElementById("toggle");
+    if (window.innerWidth > 715) {
+        toggle.checked = true;
+        document.getElementById('side-nav-header').style.visibility='visible';
+    }
+    let side_nav = document.getElementById('side-nav');
+    let container = document.getElementById('container');
+}
