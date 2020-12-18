20bf4195613da41265ee435fd378f56c4f1fd3c7
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-12-19 01:22:51 +0530

    Animations improvement

--- js/style.js
+++ js/style.js
@@ -5,7 +5,6 @@ window.onload = function() {
         document.getElementById('side-nav-header').style.visibility='visible';
     }
     let side_nav = document.getElementById('side-nav');
-    let container = document.getElementById('container');
     window.addEventListener('resize', function () { 
         if (window.innerWidth > 715) {
             toggle.checked = true;
@@ -21,16 +20,29 @@ window.onload = function() {
         return(
             bounding.top <= (window.innerHeight || document.documentElement.clientHeight)-100 &&
             bounding.left >= 0 &&
-            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+            bounding.bottom >50
+        );
+    };
+    ScreenOutDown = function(div){
+        var bounding = div.getBoundingClientRect();
+        return(
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
+    };
+    ScreenOutUp = function(div){
+        var bounding = div.getBoundingClientRect();
+        return(
+            bounding.top > 0
         );
     };
     animateRight = function(elem){
         if(toggle.checked)
         {
             if (inViewport(elem)) {
-                elem.setAttribute("style", "visibility: visible;margin-left: 0px;transition: all 1s;");
+                elem.setAttribute("style", "visibility: visible;transform: translateX(0px);transition: all 1s;");
             } else {
-                elem.setAttribute("style", "visibility: hidden;margin-left: -50px;transition: all 1s;opacity:0;");
+                elem.setAttribute("style", "visibility: hidden;transform: translateX(-50px);transition: all 1s;opacity:0;");
             }
         }
         else{
@@ -43,11 +55,12 @@ window.onload = function() {
     };
     animateUp = function(elem){
         if(inViewport(elem)){
-            elem.setAttribute("style", "visibility: visible;margin-top: 10px;transition: all 1s;");
+            elem.setAttribute("style", "visibility: visible;transform: translateY(0px);transition: all 1s;");
         }
-        else{
-            elem.setAttribute("style", "visibility: hidden;margin-top: 50px;transition: all 1s;opacity:0;");
+        else {
+            elem.setAttribute("style", "visibility: hidden;transform: translateY(40px);transition: all 1s;opacity:0;");
         }
+        
     }
     animateBar = function(elem,width){
         if(inViewport(elem)){
@@ -57,7 +70,7 @@ window.onload = function() {
             elem.setAttribute("style", `overflow:hidden;width: 10%;transition: all 1s;opacity:0;`);
         }
     }
-    window.addEventListener('scroll', function () {
+    function animate(){
         animRht = document.querySelectorAll('.animateRight');
         for(var i=0;i<animRht.length;i++){
             animateRight(animRht[i])
@@ -70,6 +83,39 @@ window.onload = function() {
         for(var i=0;i<animBar.length;i++){
             animateBar(animBar[i],animBar[i].getAttribute('aria-valuenow'))
         }
+    }
+    function pageSlider(e){
+        fullPage = document.querySelectorAll('.container-body');
+        container = document.getElementById('container');
+        var scrollHeight = window.innerHeight;
+        if (e.deltaY>0) {
+            for (let i = 0; i < fullPage.length-1; i++) {
+                const element = fullPage[i];
+                if (ScreenOutDown(fullPage[i])) {
+                    var height = fullPage[i+1].getBoundingClientRect().top;
+                    window.scrollBy({top: height, behavior: 'smooth'});
+                }
+                else{
+                    window.scrollBy({top:20, behavior: 'smooth'});
+                }
+            }
+        }
+        else{
+            for (let i = fullPage.length-1; i > 0 ; i--) {
+                const element = fullPage[i];
+                if (ScreenOutUp(fullPage[i])) {
+                    var height = fullPage[i-1].getBoundingClientRect().top;
+                    window.scrollBy({top: height, behavior: 'smooth'});
+                    
+                }
+            }
+        }
+    }
+    window.addEventListener('scroll', function (e) {
+        animate();
+    });
+    window.addEventListener('wheel', function(e){
+        //pageSlider(e);
     });
     var edu = document.querySelectorAll(".collapse");
         for(var i = 0;i<edu.length;i++){
@@ -80,5 +126,6 @@ window.onload = function() {
             
         });
     }
+
     
 }
